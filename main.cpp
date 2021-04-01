e40747805d450517ebc932da570fa07f179fdd8b
eliav-code <51802186+eliav-code@users.noreply.github.com>
2021-04-01 23:57:33 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,18 +13,16 @@ Write your code in this editor and press "Run" button to compile and execute it.
 using namespace std;
 int main()
 {
-	cout << "enter 2 words\n";
+	//cout << "enter 2 words\n";
 	int n = 0,index;
 	char a1[50], b1[50], tav;
 	cin >> a1;
 	//cout << endl;
 	cin >> b1;
 	//cout << endl;
-	cout<<"enter an index\n";
+	//cout<<"enter an index\n";
 	cin >> n;
 	myString a(a1), b(b1);
-	a.eqtingBigSmall();
-	b.eqtingBigSmall();
 	if (a > b)
 		cout << "a>b" << endl;
 	else
@@ -34,18 +32,17 @@ int main()
 		else
 			cout << "a=b" << endl;
 	}
-	if (n > strlen(a1))
-	{
-		cout << "ERROR\n";
-		for (int i = 0; i < strlen(a1); i++)
-			*(a1 + i) = 0;
-		*a1 = NULL;
-	}
+	//if (n > strlen(a1))
+	//{
+	//	cout << "ERROR\n";
+		//for (int i = 0; i < strlen(a1); i++)
+		//	*(a1 + i) = 0;
+		//*a1 = NULL;
+	//}
 	myString NewString = a.insert(n, b.getString());
 	NewString.print();
 	cin >> tav >> index;
 	NewString[index] = tav;
 	NewString.print();
-
 	return 0;
 }
